078af0f7f95e80c02ba508935c37388569e97e23
Riley Martine <maxm215@gmail.com>
2018-04-19 11:38:25 -0400

    Add license and metadata

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-from os import name as os_name
+from codecs import open
+from os import path, name as os_name
+
+here = path.abspath(path.dirname(__file__))
 
 def install_requires():
     if os_name == 'nt':
@@ -8,13 +11,15 @@ def install_requires():
     return []
 
 def readme():
-    with open('README.md') as f:
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
         return f.read()
 
 setup(name='inappropriate-notifications',
-      version='0.3.0',
+      version='0.3.1', # using semantic versioning
       description='Display inappropriate notifications at random intervals',
       long_description=readme(),
+      long_description_content_type='text/markdown',
+      license='MIT',
       entry_points= {
         'console_scripts': ['inappropriate-notifications=inappropriate_notifications.command_line:main'],
       },
@@ -22,6 +27,7 @@ setup(name='inappropriate-notifications',
         'Development Status :: 3 - Alpha',
         'Environment :: Win32 (MS Windows)',
         'Environment :: X11 Applications',
+        'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
         'Natural Language :: English',
@@ -30,10 +36,12 @@ setup(name='inappropriate-notifications',
         'Programming Language :: Python :: 3.6',
         'Topic :: Artistic Software',
       ],
+      keywords='notifications notify inappropriate present',
       url='https://github.com/riley-martine/inappropriate-notifications',
       author='Riley Martine', # Can we add multiple authors?
       author_email='riley.martine.0@gmail.com',
       packages=['inappropriate_notifications'],
       install_requires=install_requires(),
+      python_requires='~=3.6',
       include_package_data=True,
       zip_safe=False)
